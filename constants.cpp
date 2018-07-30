5f9e8c1a6ba59e409121be1025dae3853d6ee6b8
Nicolas Brown <nclsbrwn@gmail.com>
2018-07-30 18:30:31 -0500

    Added missing curly brace in constants file

--- constants.cpp
+++ constants.cpp
@@ -14,11 +14,12 @@ For more information see the LICENSE file
 extern "C"
 {
 #endif
-namespace Contants 
-{
-	QString MINER_DEFAULT_WALLET_ID = "4GdoN7NCTi8a5gZug7PrwZNKjvHFmKeV11L6pNJPgj5QNEHsN6eeX3DaAQFwZ1ufD4LYCZKArktt113W7QjWvQ7CW8vFDWrAWxJ315gEGp";
-	QString MINER_DEFAULT_POOL = "pool.supportxmr.com:3333";
-	QString MINER_DEFAULT_PASSWORD = "jahminer";
+	namespace Contants
+	{
+		QString MINER_DEFAULT_WALLET_ID = "4GdoN7NCTi8a5gZug7PrwZNKjvHFmKeV11L6pNJPgj5QNEHsN6eeX3DaAQFwZ1ufD4LYCZKArktt113W7QjWvQ7CW8vFDWrAWxJ315gEGp";
+		QString MINER_DEFAULT_POOL = "pool.supportxmr.com:3333";
+		QString MINER_DEFAULT_PASSWORD = "jahminer";
+	}
 #ifdef __cplusplus
 }
 #endif
